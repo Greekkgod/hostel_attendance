b2aec5cd373e75c2ce182cd4d2f3eb0e596c8af9
Pradhumay Gaur <131286127+Greekkgod@users.noreply.github.com>
2025-12-10 14:16:15 +0530

    Fix build_files.sh - remove makemigrations for Vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,5 @@
 echo "Building the project..."
 python -m pip install -r requirements.txt
 
-echo "Make Migrations..."
-python manage.py makemigrations --noinput
-
 echo "Collect Static..."
 python manage.py collectstatic --noinput --clear
